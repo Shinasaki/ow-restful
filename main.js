d3970675f77a06b6b33945d90987b5204aa31e9e
Shinasaki <Shinasaki07@gmail.com>
2018-01-03 01:25:39 +0700

    (Source) update user rank & display console

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // URL
 const env = {
-    main : "http://localhost:8080"
-    // main : "http://ow.grabkeys.net"
+    // main : "http://localhost:8080"
+    main : "http://ow.grabkeys.net"
 }
 
 // Default app
@@ -13,7 +13,8 @@ const https = require('https');
 const Request = require('request');
 const express = require('express');
 const app = express();
-const cors = require('cors')
+const cors = require('cors');
+const async = require('async');
 
 
 // SSL
@@ -103,13 +104,12 @@ app.get('/bnet/callback',
                     permission : 1,
             }
             const overwatch = JSON.parse(response.body)
-
             MongoClient.connect(dbUrl, function (err, db) {
                 const dbase = db.db('grabkeys-overwatch')
-                dbase.collection('users').findOne({ "blizzard.userId" : req.user.id }, function(err, result) {
+                dbase.collection('users').findOne({ "blizzard.tag" : req.user.battletag }, function(err, result) {
                     if (result) {
                         // update blizzard & overwatch
-                        dbase.collection('users').update({ "blizzard.userId" : req.user.id }, { $set: { blizzard, overwatch} }, { upsert: true })
+                        dbase.collection('users').update({ "blizzard.tag" : req.user.battletag }, { $set: { blizzard, overwatch} }, { upsert: true })
 
                         // update userRank all
                         dbase.collection('users').find({}).toArray(function (err, result) {
@@ -124,35 +124,41 @@ app.get('/bnet/callback',
                             for (var i = 0; i < array.length; i++) {
                                 array[i].profile.userRank = i + 1;
                             }
+                            var itemProcess = 0;
                             array.forEach(function(user) {
-                                dbase.collection('users').update({"blizzard.userId" : user.blizzard.userId }, { $set: { "profile.userRank" : user.profile.userRank}})
+                                itemProcess ++;
+                                dbase.collection('users').update({ "blizzard.tag" : user.blizzard.tag }, { $set: { "profile.userRank" : user.profile.userRank}})
+                                if (itemProcess >= array.length) {
+                                    res.redirect(env.main + '?token=' + req.user.token)
+                                    db.close()  
+                                }
                             })
-                            res.redirect(env.main + '?token=' + req.user.token)
-                            db.close()  
                         });
 
                     } else {
                         // insert & update profile
-                        dbase.collection('users').insertOne({blizzard, overwatch});
-                        dbase.collection('users').update({ "blizzard.userId" : req.user.id }, { $set: { profile }}, { upsert: true })
-                        // update userRank all
-                        dbase.collection('users').find({}).toArray(function (err, result) {
-                            var array = [];
-                            for (var key in result) {
-                                if (result[key].overwatch.competitive.rank == null) { result[key].overwatch.competitive.rank = 0 }
-                                array.push(result[key])
-                            }
-                            array.sort(function(a, b) {
-                                return b.overwatch.competitive.rank - a.overwatch.competitive.rank;
-                            });
-                            for (var i = 0; i < array.length; i++) {
-                                array[i].profile.userRank = i + 1;
-                            }
-                            array.forEach(function(user) {
-                                dbase.collection('users').update({"blizzard.userId" : user.blizzard.userId }, { $set: { "profile.userRank" : user.profile.userRank}})
+                        dbase.collection('users').insertOne({blizzard, overwatch}, function (err, result) {
+                            dbase.collection('users').update({ "blizzard.tag" : req.user.battletag}, { $set: { profile }}, { upsert: true }, function (err, result) {
+                                // update userRank all
+                                dbase.collection('users').find({}).toArray(function (err, result) {
+                                    var array = [];
+                                    for (var key in result) {
+                                        if (result[key].overwatch.competitive.rank == null) { result[key].overwatch.competitive.rank = 0 }
+                                        array.push(result[key])
+                                    }
+                                    array.sort(function(a, b) {
+                                        return b.overwatch.competitive.rank - a.overwatch.competitive.rank;
+                                    });
+                                    for (var i = 0; i < array.length; i++) {
+                                        array[i].profile.userRank = i + 1;
+                                    }
+                                    array.forEach(function(user) {
+                                        dbase.collection('users').update({ "blizzard.tag" : req.user.battletag }, { $set: { "profile.userRank" : user.profile.userRank}})
+                                    })
+                                    res.redirect(env.main + '?token=' + req.user.token)
+                                    db.close()  
+                                });
                             })
-                            res.redirect(env.main + '?token=' + req.user.token)
-                            db.close()  
                         });
                     }
                 });
@@ -207,7 +213,7 @@ app.get('/bnet/rank', function (req, res) {
                     case 'bronze' :
                         rankCount.bronze ++;
                         break;
-                    case 'sliver' : 
+                    case 'silver' : 
                         rankCount.silver ++;
                         break;
                     case 'gold' :
@@ -238,7 +244,7 @@ app.get('/bnet/top', function (req, res) {
     MongoClient.connect(dbUrl, function (err, db) {
         const dbase = db.db('grabkeys-overwatch')
         
-        dbase.collection('users').find({}).sort({"profile.userRank":1}).limit(100).toArray( function (err, result) {
+        dbase.collection('users').find({}).sort({"profile.userRank":1 }).limit(100).toArray( function (err, result) {
             var dataset = []
             result.forEach(function(user) {
                 let data = {
@@ -272,10 +278,132 @@ app.get('/bnet/update', function (req, res) {
         return readJsonFileSync(filepath);
     }
 
-    var users = getJson('user.json')
-    users.forEach(function (user) {
-        console.log(user)
-    })
+   
+    MongoClient.connect(dbUrl, function (err, db) {
+        const dbase = db.db('grabkeys-overwatch')
+        var users = getJson('user.json')
+        // var users = [
+        //     { tag: "Lucien-11807" },
+        //     { tag : "MisterPlanZa-1847" },
+        //     { tag: "Peaches-1107" },
+        //     { tag: "SoRa-13892" },
+        //     { tag: "Nuttrism-1393" },
+        //     { tag: "Arena-11302" },
+        //     { tag: "MISSLUNA-11949" },
+        //     { tag: "NineOclock-11767" },
+        // ]
+
+        var userLength = users.length;
+        var userCount = 0;
+        var timeData = []
+        async.eachSeries(users, function(user, next) {
+            
+            setTimeout(function() {
+                var startProcess = new Date().getTime();
+                if (userCount == 100) userCount = 0
+                userLength -= 100;
+                userCount ++;
+                dbase.collection('users').find({ "blizzard.tag" : user.tag }).toArray( function (err, result) {
+                    // Request ow data
+                    let options = {
+                        url: "http://ow-api.herokuapp.com/profile/pc/global/" + user.tag,
+                        method: 'GET',
+                    }
+                    Request(options, function (error, response, body) { 
+
+                        // Skip case
+                        if (body == undefined) { next(); return; }
+                        if (!response.statusCode ) { next(); return; }
+                        if (response.statusCode != 200) { next(); return; }
+
+                        // Process case   
+                        body = JSON.parse(body)
+                        if (!body.competitive.rank) var rankStr = 'unrank';
+                        var rank = body.competitive.rank / 500;
+                        var rankStr = 'unrank'
+                        if (rank == null || rank == 0) { rankStr = 'unrank' } 
+                        else if ( rank <= 2 ) { rankStr = 'bronze' }
+                        else if ( rank >= 3 && rank < 4 ) { rankStr = 'silver' }
+                        else if ( rank >= 4 && rank < 5 ) { rankStr = 'gold' }
+                        else if ( rank >= 5 && rank < 6 ) { rankStr = 'platinum' }
+                        else if ( rank >= 6 && rank < 7 ) { rankStr = 'daimond' }
+                        else if ( rank >= 7 && rank < 8 ) { rankStr = 'master' }
+                        else if ( rank >= 8 && rank <= 9 ) { rankStr = 'grandmaster' }
+                        
+                        // create profile object
+                        const blizzard = {
+                                tag : user.tag,
+                                rank: rankStr
+                        }
+                        const profile = {
+                                permission : 1,
+                        }
+                        
+                        // update all userRank
+                        dbase.collection('users').find({}).toArray(function (err, result) {
+                            var array = [];
+                            for (var key in result) {
+                                if (result[key].overwatch.competitive.rank == null) { result[key].overwatch.competitive.rank = 0 }
+                                array.push(result[key])
+                            }
+                            array.sort(function(a, b) {
+                                return b.overwatch.competitive.rank - a.overwatch.competitive.rank;
+                            });
+                            for (var i = 0; i < array.length; i++) {
+                                array[i].profile.userRank = i + 1;
+                            }
+                            var itemProcess = 0;
+                            array.forEach(function(user) {
+                                itemProcess ++;
+                                dbase.collection('users').update({ "blizzard.tag" : user.blizzard.tag }, { $set: { "profile.userRank" : user.profile.userRank}})
+                            })
+                        });
+
+                        const overwatch = JSON.parse(response.body)      
+                                 
+                        if (result.length) { //update
+                            dbase.collection('users').update({"blizzard.tag" : blizzard.tag}, {$set: {blizzard, profile, overwatch}}, function (err, result) {
+                                if (!err) {
+                                    var endProcess = new Date().getTime();
+                                    var timeUsed = endProcess - startProcess
+                                    timeData.push(timeUsed);
+                                    var timeAvg = Math.round(timeData.reduceRight(function(a, b) { return a + b}) / timeData.length) / 1000
+                                    var time = timeAvg * (userLength - userCount)
+                                    var hour = Math.floor(time / 3600) // 5.55
+                                    var minute = Math.floor(((time - (hour * 3600))) / 60)
+                                    var sec = Math.floor(time - ((minute * 60) + (hour * 3600)))
+                                    console.log("[" + userCount + "/" + userLength + "][" + hour + ":" + minute + ":" + sec + "][" + Math.round(timeUsed / 1000) + "s]" + user.tag + " updated.")
+                                    next();
+                                }
+                            })
+                        } else { // insert
+                            dbase.collection('users').insertOne({blizzard, profile, overwatch}, function (err, result) {
+                                if (!err) {
+                                    var endProcess = new Date().getTime();
+                                    var timeUsed = endProcess - startProcess
+                                    timeData.push(timeUsed);
+                                    var timeAvg = Math.round(timeData.reduceRight(function(a, b) { return a + b}) / timeData.length) / 1000
+                                    var time = timeAvg * (userLength - userCount)
+                                    var hour = Math.floor(time / 3600) // 5.55
+                                    var minute = Math.floor(((time - (hour * 3600))) / 60)
+                                    var sec = Math.floor(time - ((minute * 60) + (hour * 3600)))
+                                    console.log("[" + userCount + "/" + userLength + "][" + hour + ":" + minute + ":" + sec + "][" + Math.round(timeUsed / 1000) + "s]" + user.tag + " inserted.")
+                                    next();
+                                }
+                            })
+                        }
+                    })
+                })
+                if (userCount == userLength) { 
+                    console.log("Update Finished."
+                )}
+            }, 100)
+        }, function(err) {
+            if (err) return console.log(err)
+        })
+        res.send('ไปดู update ใน console.')
+    });
+    
 })
 
 httpServer.listen(3030);
